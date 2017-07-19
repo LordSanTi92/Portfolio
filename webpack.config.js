7e8d8297763f9f3ef4c3d34806a57e3c3aa213a7
Jakub Zebrowski <j.zebrowski92@gmail.com>
2017-07-19 22:47:47 +0200

    first upload files

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  entry:['whatwg-fetch', "./js/app.js"],
+  output: {
+    filename: "./js/out.js"
+  },
+  devServer: {
+    inline: true,
+    contentBase: './',
+    port: 3001
+  },
+  watch: true,
+  module: {
+    loaders: [{
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: { presets: ['es2017','es2015','stage-2', 'react'] }
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        loader: 'url-loader',
+      },
+      {
+        test: /\.(ttf|eot|woff|woff2|svg)$/,
+          loader: 'file-loader',
+            options: {
+                name: 'fonts/[name].[ext]',
+              }
+    },
+    ]
+  }
+}
